548cebf1152074363479eeff563ae3cf443de990
clv <micromath@office>
2021-06-21 09:00:09 +0800

    auto commit by make

--- labels.pl
+++ labels.pl
@@ -1,24 +1,8 @@
-# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# LaTeX2HTML 2012 (1.2)
 # Associate labels original text with physical files.
 
 
 $key = q/cite_apnwn/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_lv2017negbf/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_lv2017nepps/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_lv2018intrepqr/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_lv2018negbf8/;
 $external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
 
@@ -38,22 +22,38 @@ $key = q/cite_nrcf/;
 $external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/cite_lv2017negbf/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/cite_rcf/;
 $external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/co/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
+$key = q/cite_lv2018negbf8/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lv2017nepps/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/home/;
 $external_labels{$key} = "$URL/" . q|node1.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/co/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lv2018intrepqr/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
 1;
 
 
-# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# LaTeX2HTML 2012 (1.2)
 # labels from external_latex_labels array.
 
 
