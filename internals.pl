7f70f7e2f456e49c185c6ec07a1205c3eed54fc8
clv <micromath@sd>
2023-03-09 18:46:59 +0800

    auto commit by make

--- internals.pl
+++ internals.pl
@@ -1,8 +1,28 @@
-# LaTeX2HTML 2012 (1.2)
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
 # Associate internals original text with physical files.
 
 
 $key = q/cite_apnwn/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_hlstackc/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lv2017negbf/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lv2017nepps/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lv2018intrepqr/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lv2018negbf8/;
 $ref_files{$key} = "$dir".q|node2.html|; 
 $noresave{$key} = "$nosave";
 
@@ -18,19 +38,11 @@ $key = q/cite_lv2desc/;
 $ref_files{$key} = "$dir".q|node2.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/talk/;
-$ref_files{$key} = "$dir".q|node3.html|; 
+$key = q/cite_lvcdext/;
+$ref_files{$key} = "$dir".q|node2.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_nrcf/;
-$ref_files{$key} = "$dir".q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_lv2017negbf/;
-$ref_files{$key} = "$dir".q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_lv2018negbf8/;
 $ref_files{$key} = "$dir".q|node2.html|; 
 $noresave{$key} = "$nosave";
 
@@ -38,28 +50,16 @@ $key = q/cite_rcf/;
 $ref_files{$key} = "$dir".q|node2.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_lv2017nepps/;
-$ref_files{$key} = "$dir".q|node2.html|; 
+$key = q/co/;
+$ref_files{$key} = "$dir".q|node4.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/home/;
 $ref_files{$key} = "$dir".q|node1.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_lvcdext/;
-$ref_files{$key} = "$dir".q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_hlstackc/;
-$ref_files{$key} = "$dir".q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/co/;
-$ref_files{$key} = "$dir".q|node4.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_lv2018intrepqr/;
-$ref_files{$key} = "$dir".q|node2.html|; 
+$key = q/talk/;
+$ref_files{$key} = "$dir".q|node3.html|; 
 $noresave{$key} = "$nosave";
 
 1;
--- labels.pl
+++ labels.pl
@@ -1,8 +1,28 @@
-# LaTeX2HTML 2012 (1.2)
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
 # Associate labels original text with physical files.
 
 
 $key = q/cite_apnwn/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_hlstackc/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lv2017negbf/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lv2017nepps/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lv2018intrepqr/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lv2018negbf8/;
 $external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
 
@@ -18,19 +38,11 @@ $key = q/cite_lv2desc/;
 $external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/talk/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
+$key = q/cite_lvcdext/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_nrcf/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_lv2017negbf/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_lv2018negbf8/;
 $external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
 
@@ -38,34 +50,22 @@ $key = q/cite_rcf/;
 $external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_lv2017nepps/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
+$key = q/co/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/home/;
 $external_labels{$key} = "$URL/" . q|node1.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_lvcdext/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_hlstackc/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/co/;
-$external_labels{$key} = "$URL/" . q|node4.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_lv2018intrepqr/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
+$key = q/talk/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
 1;
 
 
-# LaTeX2HTML 2012 (1.2)
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
 # labels from external_latex_labels array.
 
 
